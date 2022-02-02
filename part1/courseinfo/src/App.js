f3ba527f91bd8930051f30de12b6d9ab19a42d87
Zachary Rais-Norman <zraisnorman@gmail.com>
2022-02-02 13:04:25 -0500

    1.2 complete

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -34,19 +34,21 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-      <p>
-        {props.p1} {props.e1}
-      </p>
-      <p>
-        {props.p2} {props.e2}
-      </p>
-      <p>
-        {props.p3} {props.e3}
-      </p>
+      <Part p={props.p1} e={props.e1} />
+      <Part p={props.p2} e={props.e2} />
+      <Part p={props.p3} e={props.e3} />
     </>
   );
 };
 
 const Total = (props) => {
   return <p>Number of exercises {props.e1 + props.e2 + props.e3}</p>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.p} {props.e}
+    </p>
+  );
 };
