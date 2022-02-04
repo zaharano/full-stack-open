f7a02571b220fadcaed768540159ebe7936330af
Zachary Rais-Norman <zraisnorman@gmail.com>
2022-02-03 19:05:12 -0500

    1.14 completed

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -20,16 +20,55 @@ const App = () => {
   ];
 
   const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(Array(anecdotes.length).fill(0));
+
+  const idxOfTopVoted = points.reduce(
+    (max, x, i, arr) => (x > arr[max] ? i : max),
+    0
+  );
+
+  const next = () => {
+    setSelected(getRandomInt(anecdotes.length));
+  };
+
+  const vote = () => {
+    let newPoints = [...points];
+    newPoints[selected] += 1;
+    setPoints(newPoints);
+  };
 
   return (
     <div className='container'>
-      <p>{anecdotes[selected]}</p>
-      <button
-        onClick={() => {
-          setSelected(getRandomInt(anecdotes.length));
-        }}>
-        Hit me again, Johnny!
-      </button>
+      <Header text="Today's Wisdom" />
+      <Anecdote text={anecdotes[selected]} points={points[selected]} />
+      <div className='line'>
+        <Button onClick={vote} text="Oh that's deep" />
+        <Button onClick={next} text='Gimme new wisdom' />
+      </div>
+      <Header text='Top Wisdom' />
+      <Anecdote
+        text={anecdotes[idxOfTopVoted]}
+        points={points[idxOfTopVoted]}
+      />
+    </div>
+  );
+};
+
+const Header = ({ text }) => {
+  return <h1>{text}</h1>;
+};
+
+const Button = ({ text, onClick }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const Anecdote = ({ text, points }) => {
+  return (
+    <div className='anecdote'>
+      <p>{text}</p>
+      <p>
+        has <strong>{points}</strong> votes
+      </p>
     </div>
   );
 };
